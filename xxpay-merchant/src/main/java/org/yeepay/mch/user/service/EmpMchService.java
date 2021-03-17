97434f5a02ff57673eaf6a1827ac49ddd3c28a27
liuhongyuan <343667924@qq.com>
2021-03-17 18:48:51 +0800

    增加授权登录

--- xxpay-merchant/src/main/java/org/yeepay/mch/user/service/EmpMchService.java
+++ xxpay-merchant/src/main/java/org/yeepay/mch/user/service/EmpMchService.java
@@ -1,9 +1,11 @@
 package org.yeepay.mch.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.yeepay.core.entity.EmpMch;
 import org.yeepay.mch.common.service.RpcCommonService;
 
+@Component
 public class EmpMchService {
     @Autowired
     private RpcCommonService rpcCommonService;
